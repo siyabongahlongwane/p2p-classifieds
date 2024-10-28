dfda2916acae890837fb5004cbaafd737c28c83d
Siyabonga Hlongwane <hlongwanesiyabonga6@gmail.com>
2024-10-28 15:10:14 +0200

    feat: SignIn UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import ProductList from "./components/Products/ProductList";
+import SignIn from "./pages/Auth/SignIn";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<ProductList />}></Route>
+          <Route path="/sign-in" element={<SignIn />}></Route>
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/pages/Auth/SignIn.tsx
@@ -0,0 +1,129 @@
+import { Box, Button, Grid2, Stack, TextField, Typography } from '@mui/material'
+import { useState } from 'react'
+import './Auth.css'
+
+const SignIn = () => {
+  const [selectedLoginMethod, setSelectedLoginMethod] = useState('pwd')
+
+  return (
+    <Grid2
+      display={'grid'}
+      container
+      gridTemplateColumns={'1fr 1fr'}
+      height={'inherit'}
+    >
+      <Stack display={'flex'} alignItems={'center'}>
+        <Stack width={'60%'} gap={2} mt={'25%'}>
+          <Stack display={'flex'} gap={0.5}>
+            <Typography variant='h5'>Welcome back!</Typography>
+            <Typography fontWeight={400} variant='body2' color='gray'>
+              Select Sign In method below
+            </Typography>
+          </Stack>
+          <Box
+            display={'flex'}
+            width={'100%'}
+            border='1px solid gray'
+            borderRadius={'10px'}
+          >
+            <Box
+              className={
+                'pointer ' +
+                (selectedLoginMethod == 'pwd' ? 'selected-method' : '')
+              }
+              onClick={() => setSelectedLoginMethod('pwd')}
+              textAlign={'center'}
+              width={'100%'}
+              p={0.5}
+              borderRadius={2}
+            >
+              <Typography>Password</Typography>
+            </Box>
+            <Box
+              className={
+                'pointer ' +
+                (selectedLoginMethod == 'otp' ? 'selected-method' : '')
+              }
+              onClick={() => setSelectedLoginMethod('otp')}
+              textAlign={'center'}
+              width={'100%'}
+              p={0.5}
+              borderRadius={2}
+            >
+              <Typography>OTP</Typography>
+            </Box>
+          </Box>
+          <Box
+            component={'form'}
+            display={'flex'}
+            flexDirection={'column'}
+            gap={2}
+          >
+            <Stack gap={1}>
+              <Typography variant='subtitle2'>Email</Typography>
+              <TextField
+                error={false}
+                placeholder='Enter Email'
+                helperText=''
+              />
+            </Stack>
+            {selectedLoginMethod == 'pwd' && (
+              <Stack gap={1}>
+                <Box
+                  display={'flex'}
+                  justifyContent={'space-between'}
+                  alignItems={'center'}
+                >
+                  <Typography variant='subtitle2'>Password</Typography>
+                  <Typography
+                    variant='body2'
+                    color='gray'
+                    fontSize={12}
+                    className='pointer'
+                  >
+                    Forgot Password?
+                  </Typography>
+                </Box>
+                <TextField
+                  error={false}
+                  placeholder='Enter Password'
+                  helperText=''
+                />
+              </Stack>
+            )}
+            <Button variant='contained' className='primary-btn'>
+              Login
+            </Button>
+          </Box>
+        </Stack>
+        <Stack display={'flex'} gap={1} mt={1} alignItems={'center'}>
+          <Typography fontStyle={'oblique'} color='gray'>
+            OR
+          </Typography>
+          <img
+            className='pointer'
+            src='../../../public/google-sign-in.svg'
+            alt='Google Sign In'
+          />
+          <Typography component={'small'} color='gray' fontWeight={300}>
+            Don't have an account?
+            <Typography ml={1} component={'span'} color='#000'>
+              Sign up.
+            </Typography>
+          </Typography>
+        </Stack>
+      </Stack>
+      <Box
+        style={{
+          backgroundImage:
+            'linear-gradient(to bottom, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.3)), url(../../../public/stationery.jpg)'
+        }}
+        height={'inherit'}
+        width={'100%'}
+        borderRadius={'40px 0 0 40px'}
+      ></Box>
+    </Grid2>
+  )
+}
+
+export default SignIn;
