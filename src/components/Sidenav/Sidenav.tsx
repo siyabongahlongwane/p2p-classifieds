cd06d8eb235fb2c528b1785d058dda44077d960a
Siyabonga Hlongwane <hlongwanesiyabonga6@gmail.com>
2024-10-28 17:09:23 +0200

    feat: Sidenav Component, Logo and color pallete

---
+++ src/components/Sidenav/Sidenav.tsx
@@ -0,0 +1,17 @@
+import { Box, Stack } from '@mui/material';
+import Menu from '../Menu/Menu';
+import './Sidenav.css';
+
+const Sidenav = () => {
+  return (
+    <Stack className="sidenav" height={'100%'}>
+        <Box className="logo" maxWidth={'200px'}>
+            <img width={'100%'} src={'../../../public/logo-v2.png'} alt="School Thrifties Logo" />
+        </Box>
+        <Menu />
+    </Stack>
+
+  )
+}
+
+export default Sidenav
