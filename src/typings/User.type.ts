0dbba2e89b7b5dfde42fd76cc792683f6950c290
Siyabonga Hlongwane <hlongwanesiyabonga6@gmail.com>
2024-10-31 00:34:59 +0200

    feat: User type

---
+++ src/typings/User.type.ts
@@ -0,0 +1,14 @@
+export type User = {
+    user_id: number;
+    google_id: string | null;
+    first_name: string;
+    last_name: string;
+    email: string;
+    password: string;
+    location: string | null;
+    phone: string;
+    completed_registration: boolean;
+    roles: number[];
+    createdAt: string;
+    updatedAt: string;
+};
